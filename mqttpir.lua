6e5b871d3b774e5884848f622ae867ef1768011a
Artem Pastukhov <artem.pastukhov@gmail.com>
2015-06-22 11:09:02 +0300

    Move to dht C module

--- mqttpir.lua
+++ mqttpir.lua
@@ -1,45 +1,66 @@
 pir = 1
 dht22 = 2
 chipid = node.chipid()
-broker = "192.168.100.122"
 gpio.mode(pir,gpio.INT)
 
-m = mqtt.Client(chipid, 120)
-m:lwt("lwt/".. chipid, chipid .." gone offline", 0, 0)
-m:connect(broker, 1883, 0, function(conn) print("connected") end)
+m = mqtt.Client(chipid, 5)
 
-m:on("offline", function(con)
-     print ("Connecting to the broker ...")
-     m:connect(broker, 1883, 0, function(conn)
-          print("connected") 
+
+
+local function connect()
+
+m:connect("192.168.100.122", 1883, 0, function(conn)
+    m:publish("events/esp8266/".. chipid .."/status", 
+    cjson.encode({Type = "Status", SensorID = chipid, Status = 1,Heap = node.heap()}), 0, 0)          
+    if DEBUG then 
+       print("connected") 
+    end
      end)
 
+end
+
+
+
+m:lwt("events/esp8266/".. chipid .."/status", 
+cjson.encode({Type = "Status", SensorID = chipid, Status = 0}), 0, 0)
+
+connect() 
+
+
+m:on("offline", function(conn)
+     if DEBUG then 
+        print ("Connecting to the broker ...")
+     end
+     connect()
 end)
 
--- on publish message receive event
-m:on("message", function(conn, topic, data)
-  if data ~= nil then
-   print(data)
-  end  
-end)
+
 
 gpio.trig(pir,"both", function (level)
         m:publish("events/esp8266/".. chipid .. "/pir" ,
-        cjson.encode({Type = "PIR", SensorID = chipid, Status = level}) ,0,0 )
-        print(level)
+            cjson.encode({Type = "PIR", SensorID = chipid, Status = level,Heap = node.heap()}) ,0,0 )        
+        if DEBUG then 
+            print(level)
+        end
 end)
 
 
 tmr.alarm(2, 10000, 1, function ()
-      local t,h =dofile("yet-another-dht22.lc").read(dht22,true)
-      if t  and h then
-        print("Temperature: "..((t-(t % 10)) / 10).."."..(t % 10).." deg C")
-        print("Humidity: "..((h - (h % 10)) / 10).."."..(h % 10).."%")
-        print("Sendind temperature and humidity")
+      local status, temp, humi, temp_decimial, humi_decimial = dht.read(dht22)
+      
+      if status == 0 then
+        temp = tostring(temp) ..'.'.. tostring(temp_decimial):gsub('0+$','')
+        humi = tostring(humi) ..'.'.. tostring(humi_decimial):gsub('0+$','')
+
+        if DEBUG then 
+            print("Temperature: "..temp.." deg C")
+            print("Humidity: "..humi.." %")
+            print("Sendind temperature and humidity")
+        end
         m:publish("events/esp8266/".. chipid .."/temperature",
-        cjson.encode({Type = "Temperature", SensorID = chipid, Temperature = ((t-(t % 10)) / 10).."."..(t % 10)})  ,0,0) 
+            cjson.encode({Type = "Temperature", SensorID = chipid, Temperature = temp, Heap = node.heap()})  ,0,0) 
         m:publish("events/esp8266/".. chipid .."/humidity",
-        cjson.encode({Type = "Humidity", SensorID = chipid, Humidity = ((h - (h % 10)) / 10).."."..(h % 10)})  ,0,0)
+            cjson.encode({Type = "Humidity", SensorID = chipid, Humidity = humi ,Heap = node.heap()})  ,0,0)
       end
 
 end)
